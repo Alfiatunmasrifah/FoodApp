fce744affa7634ff075a04e3af63624dc42eaaf3
Alfiatunmasrifah <alfiatunmasrifah0@gmail.com>
2023-08-15 09:52:04 +0700

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by Alfiatun masrifah on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
